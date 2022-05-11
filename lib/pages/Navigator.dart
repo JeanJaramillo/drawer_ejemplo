a35f59cce0c9fba5212129d661540821906de6b8
JeanJaramillo <81722915+JeanJaramillo@users.noreply.github.com>
2022-05-11 18:36:41 -0500

    navigator ok

--- lib/pages/Navigator.dart
+++ lib/pages/Navigator.dart
@@ -35,8 +35,7 @@ class _MyHomePageState extends State<MyFirstPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.network(
-                "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3eb70ccc-e7ad-4e1a-87fa-f97df9ef1c52/d8rx6aw-7aed38dd-576b-47cc-b0b0-a24803f1f2fe.png/v1/fill/w_1024,h_576,strp/dragon_ball_z__dbz__nuevo_logo_by_saodvd_d8rx6aw-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTc2IiwicGF0aCI6IlwvZlwvM2ViNzBjY2MtZTdhZC00ZTFhLTg3ZmEtZjk3ZGY5ZWYxYzUyXC9kOHJ4NmF3LTdhZWQzOGRkLTU3NmItNDdjYy1iMGIwLWEyNDgwM2YxZjJmZS5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.oYKaNgd3mlbQkcOGM5ZCJnb4RG0bBOxr3j5rSPCpSPg"),
+            Image.asset('assets/kaio.jpg'),
             ElevatedButton(
                 onPressed: () {
                   Navigator.push(
@@ -70,7 +69,7 @@ class MySecondPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.network(
-                "https://peru21.pe/resizer/9UAuA17MjUmyLhrf-LN0Cd7y9Jw=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/RYKYUOIPM5E57DGPZISRDQJ2O4.png"),
+                "https://c.tenor.com/yHidMVxXBXkAAAAC/vegeta-insecto.gif"),
             ElevatedButton(
                 onPressed: () {
                   Navigator.push(
@@ -109,8 +108,7 @@ class MyThirdPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.network(
-                "https://depor.com/resizer/ki9PdtixqgHHtOK0W_0_n09NiSE=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/WWVWAITCRNFPXIA2NELKB6GI6E.jpg"),
+            Image.asset('assets/shengdorado.png'),
             ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
